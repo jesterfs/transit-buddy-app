f571a9660e2dd362c4cbd1cb231c19efe532eaae
Stewart Jester <jesterstewart@gmail.com>
2020-12-25 16:48:07 -0800

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+  
+  it('renders the UI as expected', () => {
+    const div = document.createElement('div')
+    const tree = renderer
+      .create(<BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      div)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+    });
+
 });
+
+
+
