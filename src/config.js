80150153a079b7e4f0e17adb23b0b12581a18fee
Stewart Jester <jesterstewart@gmail.com>
2022-08-16 22:19:23 -0700

    "fixed url_for"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    // API_ENDPOINT: `http://localhost:8000/api/`, 
-    API_ENDPOINT: `https://stormy-reef-88662.herokuapp.com/api/`,
+    //API_ENDPOINT: `http://localhost:8000/api/`, 
+    API_ENDPOINT: `https://stormy-reef-88662.herokuapp.com/`,
     
     TOKEN_KEY: 'easycal-app-auth-token'
   }
