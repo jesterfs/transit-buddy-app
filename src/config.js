589c6c42a47a93d3ac694d1cf29d38f29c676f87
jesterfs <63027978+jesterfs@users.noreply.github.com>
2023-01-22 19:43:05 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: `https://cors-anywhere.herokuapp.com/corsdemo/https://immense-hollows-19284.herokuapp.com/api/`, 
+    API_ENDPOINT: `https://justcors.com/tl_f6ce48b/https://immense-hollows-19284.herokuapp.com/api/`, 
     //API_ENDPOINT: `https://stormy-reef-88662.herokuapp.com/`,
     
     //TOKEN_KEY: 'easycal-app-auth-token'
