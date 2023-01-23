00cf22aeea6ae92abc9a7036000ae21e443d37d1
jesterfs <63027978+jesterfs@users.noreply.github.com>
2023-01-22 19:34:04 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: `https://immense-hollows-19284.herokuapp.com/api/`, 
+    API_ENDPOINT: `https://young-oasis-89121.herokuapp.com/https://immense-hollows-19284.herokuapp.com/api/`, 
     //API_ENDPOINT: `https://stormy-reef-88662.herokuapp.com/`,
     
     //TOKEN_KEY: 'easycal-app-auth-token'
