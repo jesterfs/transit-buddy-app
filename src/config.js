d888a2d2af877b8625e67453a4dba0be2542dc67
jesterfs <63027978+jesterfs@users.noreply.github.com>
2023-01-24 12:21:28 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: `https://immense-hollows-19284.herokuapp.com/api/`, 
+    API_ENDPOINT: `https://trans-bud.herokuapp.com/api/`,
+    //`https://immense-hollows-19284.herokuapp.com/api/`, 
     //API_ENDPOINT: `https://stormy-reef-88662.herokuapp.com/`,
     
     
